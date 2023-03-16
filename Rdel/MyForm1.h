508aaf21ec7ab45cf6c392907d413530b207695c
Игорь Сердюк <igorserd2909@gmail.com>
2023-03-16 17:29:12 +0700

    nabrosok okna

--- Rdel/MyForm1.h
+++ Rdel/MyForm1.h
@@ -34,6 +34,12 @@ namespace Rdel {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Panel^ panel1;
+	protected:
+	private: System::Windows::Forms::Button^ button4;
+	private: System::Windows::Forms::Button^ button3;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button1;
 
 	private:
 		/// <summary>
@@ -48,11 +54,81 @@ namespace Rdel {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm1";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->panel1 = (gcnew System::Windows::Forms::Panel());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			this->button4 = (gcnew System::Windows::Forms::Button());
+			this->panel1->SuspendLayout();
+			this->SuspendLayout();
+			// 
+			// panel1
+			// 
+			this->panel1->Controls->Add(this->button4);
+			this->panel1->Controls->Add(this->button3);
+			this->panel1->Controls->Add(this->button2);
+			this->panel1->Controls->Add(this->button1);
+			this->panel1->Location = System::Drawing::Point(0, 4);
+			this->panel1->Name = L"panel1";
+			this->panel1->Size = System::Drawing::Size(912, 465);
+			this->panel1->TabIndex = 0;
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(296, 425);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(149, 37);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Вычислить";
+			this->button1->UseVisualStyleBackColor = true;
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(451, 425);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(149, 37);
+			this->button2->TabIndex = 1;
+			this->button2->Text = L" Очистить";
+			this->button2->UseVisualStyleBackColor = true;
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button3->Location = System::Drawing::Point(606, 425);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(149, 37);
+			this->button3->TabIndex = 2;
+			this->button3->Text = L"История";
+			this->button3->UseVisualStyleBackColor = true;
+			// 
+			// button4
+			// 
+			this->button4->Font = (gcnew System::Drawing::Font(L"Calibri", 12, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button4->Location = System::Drawing::Point(760, 425);
+			this->button4->Name = L"button4";
+			this->button4->Size = System::Drawing::Size(149, 37);
+			this->button4->TabIndex = 3;
+			this->button4->Text = L"База";
+			this->button4->UseVisualStyleBackColor = true;
+			// 
+			// MyForm1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(8, 16);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->AutoValidate = System::Windows::Forms::AutoValidate::EnablePreventFocusChange;
+			this->ClientSize = System::Drawing::Size(924, 481);
+			this->Controls->Add(this->panel1);
+			this->Name = L"MyForm1";
+			this->Text = L"Rdel calculator";
+			this->panel1->ResumeLayout(false);
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
